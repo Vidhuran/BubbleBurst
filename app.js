d3e2620eff0b1c2f0149456ba84f08afeb97466f
U-EEMEA\evidhar <evidhar@EVE4115B23BCDE.eemea.ericsson.se>
2012-11-29 16:18:57 +0200

    Changed video src to getUserMedia

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 $(function() {
-    var myVideo=document.getElementById("video1"); 
+	
+	// For corss-browser support
+	window.URL = window.URL || window.webkitURL;
+	navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia || navigator.msGetUserMedia;
+	
+	// Assign the <video> element to a variable
+    var video = document.getElementById('sourcevid');
 
+	var onFailure = function(e) {
+		console.log('Reeeejected!', e);
+	};
+	
+    // Replace the source of the video element with the stream from the camera
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+			video.src = window.URL.createObjectURL(localMediaStream);
+		}, onFailure);
+	} else {
+		video.src = 'video2.ogg'; // fallback.
+	}
+	
 	$('#playPause').on('click',function ()
 	{ 
 		if (myVideo.paused) 
